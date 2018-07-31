d2080008d039a3fe6a767aebbef6bfdd912f0d0b
Phuoc Nguyen <phuoc.nguyen@metropolia.fi>
2018-07-31 14:31:42 +0300

    Passport Google Strategy Config &  environment keys implemented

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://rangigo:Phuoc9506500@ds161411.mlab.com:61411/shary-dev',
+  googleClientID: '590834941980-ksatqn1t1rcpu4rgfvbrlu9dmersj5eq.apps.googleusercontent.com',
+  googleClientSecret: 'N8TNd4l7QKFx4iBxbIaoN9dk' 
+}
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const passport = require('passport')
+
+// Passport Config
+require('./config/passport')(passport)
+
+
+// Load Routes
+const auth = require('./routes/auth')
 
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('Started')
 })
+
+
+// Use Routes
+app.use('/auth', auth)
 
 const port = process.env.PORT || 5000
 
