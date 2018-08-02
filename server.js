3cca44407312971c7d99c646ff24531ba1b4434b
Phuoc Nguyen <phuoc.nguyen@metropolia.fi>
2018-08-02 16:50:17 +0300

    Heroku production configuration

--- server.js
+++ server.js
@@ -48,6 +48,20 @@ app.use(passport.session())
 app.use('/auth', auth)
 app.use('/api/users', users)
 
+// Production Configuration
+if (process.env.NODE_ENV === 'production') {
+  // Express serves production assets (main.js or main.css)
+  app.use(express.static('client/build'))
+
+  // Express serve index.html if no routes recognized
+  const path = require('path')
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
